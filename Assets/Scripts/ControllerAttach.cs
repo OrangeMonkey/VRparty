44c168540085ab98c6d04f4950cf4408fa313e9d
James King <james.king@facepunchstudios.com>
2016-03-06 15:53:48 +0000

    Added physics materials, tweaks for VR

---
+++ Assets/Scripts/ControllerAttach.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using JetBrains.Annotations;
+
+public class ControllerAttach : MonoBehaviour
+{
+    public GameObject ToAttach;
+    public string DestChild = "trackhat";
+    
+    [UsedImplicitly]
+	private void Update()
+	{
+	    var child = transform.FindChild(DestChild);
+	    if (child == null) return;
+
+	    ToAttach.transform.SetParent(child.FindChild("attach"), false);
+
+	    Destroy(this);
+	}
+}
