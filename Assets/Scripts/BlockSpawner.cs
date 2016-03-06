19f83f46081e59e3bef3c8d6c68228d34a326cc4
Jonathan Morrell <theorangemonkey@live.co.uk>
2016-03-06 15:18:36 +0000

    Added initial project stuff I dunno.

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawner : MonoBehaviour
+{
+    private float _lastSpawnTime;
+    public float SpawnRate = 0.5f;
+    public Vector2 SpawnAreaSize = new Vector2(1f, 1f);
+    public float SpawnHeight = 20f;
+    public GameObject BlockPrefab;
+
+    public void SpawnBlock()
+    {
+        var spawnOffset = new Vector3(Random.Range(-SpawnAreaSize.x * 0.5f, SpawnAreaSize.x * 0.5f), 0f, Random.Range(-SpawnAreaSize.y * 0.5f, SpawnAreaSize.y * 0.5f));
+        var spawnPos = transform.position + Vector3.up * SpawnHeight + spawnOffset;
+        var spawnRotation = Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.up);
+
+        var newBlock = Instantiate<GameObject>(BlockPrefab);
+        newBlock.transform.position = spawnPos;
+        newBlock.transform.rotation = spawnRotation;
+        var meshRenderer = newBlock.GetComponent<MeshRenderer>();
+
+        meshRenderer.material.SetColor("_EmissionColor", Utilities.HSVToRGB(Random.Range(0f, 1f), 1, 1));
+
+        _lastSpawnTime = Time.time;
+    }
+
+    private void Update()
+    {
+        var timeSinceLastSpawn = Time.time - _lastSpawnTime;
+
+        if (timeSinceLastSpawn > 1f / SpawnRate)
+        {
+            SpawnBlock();
+        }
+    }
+}
